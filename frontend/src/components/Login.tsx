dc771492abaa9dd1bb8f9b2e24caf136d6895fde
Paz Belén Catrilaf Catrilaf <128109443+pazihs@users.noreply.github.com>
2025-10-19 23:24:20 -0300

    fix bugs about auth

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -31,7 +31,8 @@ const Login = () => {
             setUsername("");
             setPassword("");
             console.log("Logged in user:", user);
-            // navigate("/");
+            window.dispatchEvent(new Event('userChanged'));
+            navigate("/", { replace: true });
         } catch (exception) {
             setErrorMessage("Wrong credentials");
             setTimeout(() => {
@@ -46,35 +47,41 @@ const Login = () => {
     };
 
     return (
-          <div style={{ marginTop: 70, alignItems: "center", display: "flex", flexDirection: "column" }}>
+        <div style={{ marginTop: 70, alignItems: "center", display: "flex", flexDirection: "column" }}>
             <h1 style={{ textAlign: "center" }}>Iniciar sesión</h1>
             <p style={{ color: "red" }}>{errorMessage}</p>
             {!user ?
-                ( <form style={{ display: "flex", flexDirection: "column", alignItems: "center", maxWidth: 600 }} onSubmit={handleLogin}>
-                         <div>
-                    <label>Nombre de usuario</label>
-                    <input
-                        className="form-input"
-                        type="text"
-                        id="username"
-                        name="username"
-                        placeholder="Your username.."
-                        value={username}
-                        onChange={({ target }) => setUsername(target.value)}>
-                    </input>
+                (<form style={{ display: "flex", flexDirection: "column", alignItems: "center", maxWidth: 600 }} onSubmit={handleLogin}>
+                    <div>
+                        <label>Nombre de usuario</label>
+                        <input
+                            className="form-input"
+                            type="text"
+                            id="username"
+                            name="username"
+                            placeholder="Your username.."
+                            value={username}
+                            onChange={({ target }) => setUsername(target.value)}>
+                        </input>
 
-                    <label>Contraseña</label>
-                    <input
-                        className="form-input"
-                        type="password"
-                        id="password"
-                        name="password"
-                        placeholder="Your password.."
-                        value={password} onChange={({ target }) => setPassword(target.value)}>
-                    </input>
-                </div>
-                   <button className="register-button" type="submit">Iniciar Sesión</button>
-                   <button className="to-login-button" onClick={() => navigate("/register")}>Crear cuenta</button>
+                        <label>Contraseña</label>
+                        <input
+                            className="form-input"
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Your password.."
+                            value={password} onChange={({ target }) => setPassword(target.value)}>
+                        </input>
+                    </div>
+                    <button
+                        className="register-button"
+                        type="submit"
+                        onClick={() => navigate("/", { replace: true })}
+                    >
+                        Iniciar Sesión
+                    </button>
+                    <button className="to-login-button" onClick={() => navigate("/register")}>Crear cuenta</button>
                 </form>) :
                 (<p>{user.username} logged-in <button className="register-button" onClick={handleLogout}>logout</button>
                 </p>)}
--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import type { ClassroomData } from "../types/classrooms";
 import Search from "./Search";
 import { useEffect, useState } from "react";
@@ -13,24 +13,35 @@ type NavBarProps = {
 
 const NavBar = ({ setClassrooms, setQuery }: NavBarProps) => {
     const navigate = useNavigate();
-    // const location = useLocation();
+    const location = useLocation();
 
     const [user, setUser] = useState<UserData | null>(null);
+    
     useEffect(() => {
         const init = async () => {
             const user = await loginService.restoreLogin();
             setUser(user);
         };
         init();
-    }, []);
-    //const username = (location.state as { user?: string | null } | null)?.user ?? null;
+         const onUserChanged = async () => {
+            const u = await loginService.restoreLogin();
+            setUser(u);
+        };
+        window.addEventListener('userChanged', onUserChanged);
+        return () => window.removeEventListener('userChanged', onUserChanged);
+    }, [location]);
+    
     const isLoggedIn = user !== null;
     console.log("User in NavBar:", user);
     return (
         <nav className="navbar">
             <Search setClassrooms={setClassrooms} setQ={setQuery} />
             {isLoggedIn ? (
-                <button style={{ marginRight: "15px" }} onClick={() => navigate("/")}>
+                <button style={{ marginRight: "15px" }} onClick={() => {
+                    loginService.logout();
+                    setUser(null);
+                    navigate("/", { replace: true });
+                    }}>
                     Cerrar sesión
                 </button>
             ) : (
